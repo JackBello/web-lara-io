769e8e0f6a16b5818d5b1c44a549998a4181a132
JackBello <vitrollers@outlook.com>
2022-08-07 19:50:39 -0500

    👌 IMPROVE: change config server

--- configs/server.config.ts
+++ configs/server.config.ts
@@ -2,7 +2,7 @@ import { ServerConfig } from '@lara-io/types';
 import { env } from '@lara-io/helpers';
 
 export default (): ServerConfig => ({
-    hostname: env('SERVER_NAME', 'localhost'),
-    port: env('SERVER_PORT', 3000),
+    hostname: env('SERVER_NAME', ''),
+    port: env('SERVER_PORT', 80),
     transport: env('SERVER_TRANSPORT', 'tcp'),
 });
