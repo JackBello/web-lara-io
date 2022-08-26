f7857559eed89841669b896c54320a548af0c62f
JackBello <vitrollers@outlook.com>
2022-08-25 20:36:55 -0500

    👌 IMPROVE: add redirect to www 3

--- app/http/middlewares/redirect.middleware.ts
+++ app/http/middlewares/redirect.middleware.ts
@@ -1,5 +1,8 @@
 const redirectToWWW = ({ request }: any, next: any) => {
     const { baseUrl } = request();
+
+    console.log(baseUrl);
+
     if (baseUrl.includes("www")) {
         return  Response.redirect("https://www.lara-io.com", 302)
     }
