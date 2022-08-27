56f9b3c84adc7b67132a045ac1c95d9d826375a3
JackBello <vitrollers@outlook.com>
2022-08-27 13:53:58 -0500

    👌 IMPROVE: add redirect to www 12

--- app/http/middlewares/redirect.middleware.ts
+++ app/http/middlewares/redirect.middleware.ts
@@ -1,10 +1,13 @@
 import type { TRouteContext } from '@lara-io/types';
+import { env } from '@lara-io/helpers';
 
 const redirectToWWW = ({ request }: TRouteContext, next: any) => {
     const { baseUrl } = request();
 
     if (!baseUrl.includes("www")) {
-        return Response.redirect("www.lara-io.com", 302)
+        const urlRedirect = env("APP_ENV", "local") === "local" ? "www.lara-io.com" : "https://www.lara-io.com";
+
+        return Response.redirect(urlRedirect, 302)
     }
 
     next();
